723f1213a4f70d8975591e3928f5f92d600dda7c
Deepak Mahakale <deepakmahakale@gmail.com>
2021-05-08 14:03:17 +0530

    Major changes in 0.0.2.beta
    - Move list to subcommand
    - Move upgrade to subcommand
    - Nest rails as a subcommand under upgrade
    
      before: wanda rails upgrade [options]
      after:  wanda upgrade rails [options]
    
    - Method refactoring
    - Add git changes
    -

--- lib/wanda/cli.rb
+++ lib/wanda/cli.rb
@@ -9,18 +9,6 @@ module Wanda
   class CLI < Thor
     include Thor::Actions
 
-    # Keep sorted list | except rails
-    SUPPORTED_GEMS = {
-      rails: [
-        { from: 4.2, to: 5.2 },
-        { from: 5.2, to: 6.0 }
-      ]
-    }.freeze
-
-    def self.exit_on_failure?
-      true
-    end
-
     desc 'version', 'Display version'
     map %w[-v --version] => :version
     def version
@@ -30,26 +18,8 @@ module Wanda
     desc 'upgrade [GEM]', 'Upgrade gem'
     subcommand 'upgrade', Wanda::Upgrade
 
-    desc 'list', 'List supported gems'
-    def list
-      message = <<~STR
-        Supports upgrade for:
-        #{'=' * 76}
-            #{format_list}
-      STR
-      puts set_color(message, :green)
-    end
-
-    private
-
-    def format_list
-      SUPPORTED_GEMS.map do |gem, versions|
-        version_list = versions.map do |version|
-          "#{version[:from]} => #{version[:to]}"
-        end.join(', ')
-
-        "#{gem}: " + version_list
-      end.join("\n    ")
+    def self.exit_on_failure?
+      true
     end
   end
 end
--- lib/wanda/upgrade.rb
+++ lib/wanda/upgrade.rb
@@ -3,12 +3,19 @@
 require 'thor'
 require 'wanda/sub_command_base'
 require 'wanda/rails'
+require 'wanda/list'
 
 module Wanda
   class Upgrade < SubCommandBase
     include Thor::Actions
 
-    desc 'rails [options]', 'Rails upgrade'
-    subcommand 'rails', Wanda::Rails
+    # register(klass, subcommand_name, usage, description, options = {})
+    #      desc usage, description, options
+    #
+    register(Wanda::Rails, 'rails', 'rails [options]', Wanda::Rails.desc)
+    tasks["rails"].options = Wanda::Rails.class_options
+
+    register(Wanda::List, 'list', 'list [options]', Wanda::List.desc)
+    tasks["list"].options = Wanda::List.class_options
   end
 end
